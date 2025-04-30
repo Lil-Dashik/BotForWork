4ffdf9cc9e3f3cdb820ca976db17551e68dbce62
Lil-Dashik <mors_ph_sadboy@mail.ru>
2025-05-01 01:45:50 +0400

    исправлена отправка сообщения о неверном формате данных пользователю

--- backend/src/main/java/project/configuration/KafkaConfig.java
+++ backend/src/main/java/project/configuration/KafkaConfig.java
@@ -1,19 +1,30 @@
 package project.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
+import project.dto.BotCommandDTO;
 import project.dto.NotificationDTO;
 
 import java.util.Map;
-
+@Slf4j
 @Configuration
 public class KafkaConfig {
+    private final KafkaTemplate<Long, NotificationDTO> kafkaTemplate;
+    @Autowired
+    public KafkaConfig(KafkaTemplate<Long, NotificationDTO> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Bean
     public NewTopic startCommandTopic() {
         return TopicBuilder.name("bot-start")
@@ -67,19 +78,31 @@ public class KafkaConfig {
                 .configs(Map.of("min.insync.replicas", "1"))
                 .build();
     }
-    @Bean(name = "kafkaListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<Long, NotificationDTO> kafkaListenerContainerFactory(
-            ConsumerFactory<Long, NotificationDTO> consumerFactory) {
 
-        ConcurrentKafkaListenerContainerFactory<Long, NotificationDTO> factory =
+    @Bean(name = "kafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<Long, BotCommandDTO> kafkaListenerContainerFactory(
+            ConsumerFactory<Long, BotCommandDTO> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Long, BotCommandDTO> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
-        factory.setCommonErrorHandler(new DefaultErrorHandler(
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
                 (consumerRecord, exception) -> {
-                    System.err.println("Ошибка при получении сообщения: " + exception.getMessage());
+                    if (consumerRecord.key() instanceof Long telegramId) {
+                        NotificationDTO errorMsg = new NotificationDTO();
+                        errorMsg.setTelegramUserId(telegramId);
+                        errorMsg.setMessage("Ошибка в формате команды. Убедитесь, что вы отправили: <дом>; <работа>; <время>");
+                        errorMsg.setNotifyTime(null);
+
+                        kafkaTemplate.send("confirmations", telegramId, errorMsg);
+                    }
+
+                    log.warn("Ошибка при обработке сообщения Kafka: {}", exception.getMessage());
                 },
                 new FixedBackOff(0L, 0)
-        ));
+        );
+
+        factory.setCommonErrorHandler(errorHandler);
 
         return factory;
     }
