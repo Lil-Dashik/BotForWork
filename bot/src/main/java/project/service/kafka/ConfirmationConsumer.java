aa5267813af59cc98dc6565cc8f333f450b29866
Lil-Dashik <mors_ph_sadboy@mail.ru>
2025-05-01 00:28:37 +0400

    исправлена отправка сообщения о неверном формате данных пользователю

--- bot/src/main/java/project/service/kafka/ConfirmationConsumer.java
+++ bot/src/main/java/project/service/kafka/ConfirmationConsumer.java
@@ -1,11 +1,12 @@
 package project.service.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import project.dto.NotificationDTO;
 import project.service.CommuteBot;
-
+@Slf4j
 @Component
 public class ConfirmationConsumer {
     private final CommuteBot commuteBot;
@@ -18,6 +19,7 @@ public class ConfirmationConsumer {
 
     @KafkaListener(topics = "confirmations", groupId = "bot-group", containerFactory = "kafkaListenerContainerFactory")
     public void listenConfirmation(NotificationDTO notification) {
+        log.info("Получено сообщение из Kafka (confirmations): {}", notification);
         commuteBot.sendMessage(notification.getTelegramUserId(), notification.getMessage());
     }
 }
